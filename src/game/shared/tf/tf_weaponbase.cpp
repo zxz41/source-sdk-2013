cdc2c6c808604eb4fe02762065f4c29cd381bae5
Jori Kos <43441605+JoriKos@users.noreply.github.com>
2025-08-07 16:46:15 +0200

    Fixed Widowmaker not giving metal on friendly disguises

--- src/game/shared/tf/tf_weaponbase.cpp
+++ src/game/shared/tf/tf_weaponbase.cpp
@@ -4969,6 +4969,7 @@ void CTFWeaponBase::ApplyOnHitAttributes( CBaseEntity *pVictimBaseEntity, CTFPla
 		if ( pVictim && 
 			 pVictim->IsPlayerClass( TF_CLASS_SPY ) && 
 			 pVictim->m_Shared.InCond( TF_COND_DISGUISED ) && 
+			 (pVictim->m_Shared.GetDisguiseTeam() != pVictim->GetTeamNumber()) &&
 			 !( pVictim->m_Shared.IsStealthed() || pVictim->m_Shared.InCond( TF_COND_STEALTHED_BLINK ) ) )
 		{
 			flPercentage = 0.0f;
